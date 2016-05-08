ca22349c9a9d0cf8226e6e8a0f5d36248b54287e
theFerdi265 <theferdi265@gmail.com>
2016-05-08 19:13:54 +0200

    bin: rename win to wid

--- src/bin/wmove.rs
+++ src/bin/wmove.rs
@@ -22,17 +22,17 @@ fn main() {
             ("x", "x coordinate"),
         arg y: i32 = 0,
             ("y", "y coordinate"),
-        arg win: wtools::WindowID = 0.into(),
-            ("win", "XServer window id (hexadecimal)")
+        arg wid: wtools::WindowID = 0.into(),
+            ("wid", "XServer window id (hexadecimal)")
     }
 
-    cli::handle_error(&name, 1, run(mode, x, y, win));
+    cli::handle_error(&name, 1, run(mode, x, y, wid));
 }
 
-fn run(mode: Mode, x: i32, y: i32, w: wtools::WindowID) -> Result<(), &'static str> {
+fn run(mode: Mode, x: i32, y: i32, wid: wtools::WindowID) -> Result<(), &'static str> {
     let disp = try!(wtools::Display::open());
     let mut win = try!(
-        disp.window(w).map_err(|_| "window id does not exist")
+        disp.window(wid).map_err(|_| "window id does not exist")
     );
     match mode {
         Mode::Relative => try!(win.position_relative(x, y)),

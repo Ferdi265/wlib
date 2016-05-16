4bf8c423f36a03658d414f453f077af233d74bad
theFerdi265 <theferdi265@gmail.com>
2016-05-16 12:38:51 +0200

    bin: wroot to wwindow, also focus mode

---
+++ src/bin/wwindow.rs
@@ -0,0 +1,41 @@
+#[macro_use]
+extern crate wtools;
+
+use std::env;
+use wtools::cli;
+
+#[derive(Copy, Clone)]
+enum Mode {
+    Root,
+    Focus
+}
+
+fn main() {
+    let name = cli::name(&mut env::args());
+
+    parse_args!{
+        description: "returns special windows",
+        flag mode: Mode = Mode::Root,
+            (&["-r", "--root"], Mode::Root, "root window"),
+            (&["-f", "--focus"], Mode::Focus, "focused window")
+    }
+
+    cli::handle_error(&name, 1, run(mode));
+}
+
+fn run(mode: Mode) -> Result<(), &'static str> {
+    let disp = try!(wtools::Display::open());
+    let win = match mode {
+        Mode::Root => {
+            let scrn = try!(disp.screen());
+            try!(scrn.root())
+        },
+        Mode::Focus => {
+            let r = disp.focus()
+                .and_then(|wo| wo.ok_or("no window focused"));
+            try!(r)
+        }
+    };
+    println!("{}", win.id());
+    Ok(())
+}
